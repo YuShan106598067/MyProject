60c238c61b111d425e38a11b6538e12c9bfe9db7
u10216045 <janicegogogo@gmail.com>
2017-10-05 13:34:36 +0800

    Update number.h

--- number.h
+++ number.h
@@ -14,7 +14,7 @@ public:
 	Number(int s):_symbol(std::to_string(s)),_value(std::to_string(s)) {}
 	string  _symbol;
 	string value() { return _value; }
-
+	string symbol() { return _symbol; }
 	bool match(Number &a);
 	bool match(Atom &a);
 	bool match(Variable &b);
