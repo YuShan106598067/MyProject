a1d08c56e0d56fb504c2e2e17a3f29bd25696612
u10216045 <janicegogogo@gmail.com>
2017-10-19 12:34:52 +0800

    Update atom.h

--- atom.h
+++ atom.h
@@ -29,33 +29,39 @@ class Term{
 
 class Atom : public Term{
 	public:
-	  Atom (string s) :_symbol(s) , _value(s) {
+	  Atom (string s) :_symbol(s)  {
+		  *_value = s;
 	  }
 
-	  string symbol() const{
-		return _symbol;
+	  string symbol() const {
+		  return _symbol;
 	  }
 	  string value() const {
-		  return _value;
+		  return *_value;
 	  }
-
+	  string **ptr() {
+		  return &_value;
+	  }
 	  bool match(Term &a) {
 		  _assignable = true;
-		  if (a.value().length() == 0)
-			  a.value() = _symbol;
-		  if (a.value() != _value)
+		  if (_value->length() == 0) {
+			  *_value = a.value();
+		  }
+		  if (_value->length() == 0 && a.value().length() == 0) {
+			  *a.ptr() = _value;
+		  }
+		  if (a.value() != *_value)
 			  _assignable = false;
 		  return _assignable;
 	  }
-	
 
-
-	private:
-	  bool _assignable;
-	  string _symbol;
-	  string _value;
+private:
+	bool _assignable;
+	string _symbol;
+	string *_value = new string[1];
 
 };
 
 
 #endif
+
