c664ed7af3725ac61618ad3c9c11c5a6e4d805fd
u10216045 <janicegogogo@gmail.com>
2017-10-19 12:58:46 +0800

    Update number.h

--- number.h
+++ number.h
@@ -11,27 +11,23 @@ class Number : public Term {
 public:
 	Number(double s) :_symbol(std::to_string(s)){
 		ss << s;
-		*_value = ss.str();
+		_value = ss.str();
 	}
 	
 	string symbol() const {
 		return _symbol;
 	}
 	string value() const {
-		return *_value;
+		return _value;
 	}
-	string **ptr() {
-		return &_value;
-	}
+
 	bool match(Term &a) {
 		_assignable = true;
-		if (_value->length() == 0) {
-			*_value = a.value();
+		if (_value.length() == 0) {
+			_value = a.symbol();
 		}
-		if (_value->length() == 0 && a.value().length() == 0) {
-			*a.ptr() = _value;
-		}
-		if (a.value() != *_value)
+
+		if (a.value() != _value && a.value().length() != 0)
 			_assignable = false;
 		return _assignable;
 	}
@@ -39,7 +35,7 @@ public:
 private:
 	std::stringstream ss;
 	bool _assignable;
-	string *_value = new string[1];
+	string _value;
 	string  _symbol;
 
 };
