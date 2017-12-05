69e3b85f6f84913487de64f8584d3699c03452d4
u10216045 <janicegogogo@gmail.com>
2017-12-05 13:57:35 +0800

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -86,13 +86,13 @@ List * List::tail() const {
     List *ls= new List(_clone_elements) ;
     return ls;
 }
-Iterator*  List::createIterator(){
+Iterator<Term*>*  List::createIterator(){
     return new ListIterator<Term*>(this);
 }
-Iterator * List::createDFSIterator(){
+Iterator<Term*> * List::createDFSIterator(){
     return new DFSIterator<Term*>(this);
 }
   
-Iterator * List::createBFSIterator(){
+Iterator<Term*> * List::createBFSIterator(){
     return new BFSIterator<Term*>(this);
 }
