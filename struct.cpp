385ea714f0c39c01691bda7364ae25a27b45f9ec
u10216045 <janicegogogo@gmail.com>
2017-12-05 12:00:44 +0800

    Update struct.cpp

--- struct.cpp
+++ struct.cpp
@@ -2,13 +2,12 @@
 #include "iterator.h"
 Iterator * Struct::createIterator()
 {
-  return new StructIterator(this);
+  return new StructIterator<Term>(this);
 }
 Iterator * Struct::createDFSIterator(){
-  return new DFSIterator(this);
+  return new DFSIterator<Term>(this);
 }
 
 Iterator * Struct::createBFSIterator(){
-  return new BFSIterator(this);
+  return new BFSIterator<Term>(this);
 }
-
